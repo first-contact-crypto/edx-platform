f96015188530fff674108e4336caa63234b42402
Peter Alexander <peter.alexander@prodatalab.com>
2019-04-28 09:45:42 -0600

    fix badgr backend api urls

--- lms/djangoapps/badges/backends/badgr.py
+++ lms/djangoapps/badges/backends/badgr.py
@@ -35,14 +35,14 @@ class BadgrBackend(BadgeBackend):
         """
         Base URL for all API requests.
         """
-        return "{}/v1/issuer/issuers/{}".format(settings.BADGR_BASE_URL, settings.BADGR_ISSUER_SLUG)
+        return "{}/v2/issuers/{}".format(settings.BADGR_BASE_URL, settings.BADGR_ISSUER_SLUG)
 
     @lazy
     def _badge_create_url(self):
         """
         URL for generating a new Badge specification
         """
-        return "{}/badges".format(self._base_url)
+        return "{}/badgeclasses".format(self._base_url)
 
     def _badge_url(self, slug):
         """
