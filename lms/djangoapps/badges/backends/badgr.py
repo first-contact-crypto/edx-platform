d089adfd2180c3e4c2f832618c2f2de20c41aba1
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-06-09 02:12:38 -0600

    fix err 2

--- lms/djangoapps/badges/backends/badgr.py
+++ lms/djangoapps/badges/backends/badgr.py
@@ -226,11 +226,11 @@ class BadgrBackend(BadgeBackend):
         """
         Register an assertion with the Badgr server for a particular user for a specific class.
         """
-        
+
         LOGGER.info("BADGE_CLASS: In _create_assertion.. the user.username;user.email is: {} ~~ {}".format(user.username, user.email))
         data = {
             'recipient': {
-                'identity': user.email
+                'identity': user.email,
                 'type': 'email',
                 'hashed': False,
                 'plaintextIdentity': user.username
