37a21feabe66982611b6eb098c93110b958a1a27
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-06-10 05:57:27 -0600

    assertion fix 3

--- lms/djangoapps/badges/backends/badgr.py
+++ lms/djangoapps/badges/backends/badgr.py
@@ -253,7 +253,7 @@ class BadgrBackend(BadgeBackend):
 
 
         if assertion.badge_class.slug == 'course':
-            epiphany_badge_class = BadgeClass.objects.filter(badgr_server_slug=EPIPHANY_BADGR_SLUG)
+            epiphany_badge_class = BadgeClass.objects.get(badgr_server_slug=EPIPHANY_BADGR_SLUG)
             LOGGER.info("BADGE_CLASS: In _create_assertion.. epiphany_badge_class.slug: {}".format(epiphany_badge_class.slug))
             for i in range(5):
                 self._create_assertion(epiphany_badge_class, user, None)
