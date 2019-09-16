e943749ad5ec83cb3a60e846631917a84800424a
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-09-15 18:32:59 -0600

    moved migration file

--- common/djangoapps/custom_reg_form/models.py
+++ common/djangoapps/custom_reg_form/models.py
@@ -12,7 +12,7 @@ class ExtraInfo(models.Model):
     """
     user = models.OneToOneField(USER_MODEL, null=True)
 
-    promo_code2 = models.CharField(
+    promo_code = models.CharField(
         verbose_name="Promo Code",
         max_length=25,
     )
