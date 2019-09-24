6c4d43bb8c0b1ebf263fa8ec05808cf5ce0958ef
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-09-24 14:06:31 -0600

    moved validator outside class

--- common/djangoapps/custom_reg_form/models.py
+++ common/djangoapps/custom_reg_form/models.py
@@ -12,6 +12,12 @@ PROMO_CODES = [
 
 NUM_EPIPH_AWARDED = 5
 
+def validate_promo_code(value):
+    if value in PROMO_CODES:
+        bc = BadgeClass.objects.get(badgr_server_slug='CM-sak0wQuCty2BfSEle3A')
+        be = BadgrBackend()
+        for i in len(NUM_EPIPH_AWARDED):
+            be.award(bc, self.user)
 
 
 class ExtraInfo(models.Model):
@@ -20,7 +26,7 @@ class ExtraInfo(models.Model):
     The form that wraps this model is in the forms.py file.
     """
     user = models.OneToOneField(USER_MODEL, null=True)
-    promo_code = models.CharField(verbose_name="Promo Code", validators=[self.validate_promo_code], max_length=25, blank=True, null=True)
+    promo_code = models.CharField(verbose_name="Promo Code", validators=[validate_promo_code], max_length=25, blank=True, null=True)
 
     def __str__(self):
         return self.promo_code
@@ -29,9 +35,3 @@ class ExtraInfo(models.Model):
         self.promo_code = self.promo_code.strip().upper()
         super(State, self).save(force_insert, force_update)
 
-    def validate_promo_code(value):
-        if value in PROMO_CODES:
-            bc = BadgeClass.objects.get(badgr_server_slug='CM-sak0wQuCty2BfSEle3A')
-            be = BadgrBackend()
-            for i in len(NUM_EPIPH_AWARDED):
-                be.award(bc, self.user)
