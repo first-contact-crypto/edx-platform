66839d1fd571490fa9141dd825536526e269060b
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-11-14 18:23:40 -0700

    bat

--- common/djangoapps/student/views/dashboard.py
+++ common/djangoapps/student/views/dashboard.py
@@ -70,7 +70,7 @@ from badges.backends.badgr import BadgrBackend
 
 LOG = logging.getLogger("edx.student")
 
-BADGR_ACCESS_TOKEN = 'DPP7EfHiZi0bYpjtwPh6uyxotQCKWO'
+BADGR_ACCESS_TOKEN = '2YarGy7vhmne6O94NDPX9IuOuu3LSD'
 
 BADGR_SERVER_SLUG_EPIPHANY = "CM-sak0wQuCty2BfSEle3A"
 BADGR_SERVER_SLUG_COURSE = "RBNmTgTUTQC4o_0-yDIA4g"
